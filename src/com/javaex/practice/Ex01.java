976fb948b8403b07adb2c7e90e92d50e6f3bb450
Mocking_jayk0918 <jesper2able@gmail.com>
2022-04-26 18:19:29 +0900

    1.1(missing commits 22.04.26 18:20)

--- src/com/javaex/practice/Ex01.java
+++ src/com/javaex/practice/Ex01.java
@@ -8,20 +8,20 @@ public class Ex01 {
 		// String[] agrgs로 표기되어 있으므로 전부 String[] args로 수정요망
 		
 		//int true; // true -> 예약어 사용불가
-		int True;
+		//int True;
 		
 		//int #_of_workers; //특수문자 사용불가($, _ 제외)
-		int _of_workers;
+		//int _of_workers;
 		
-		int countOfLetterslnString; //pass
+		//int countOfLetterslnString; //pass
 		
 		//int 1stLevel1; // 숫자로 시작 불가
-		int stLevel1;
+		//int stLevel1;
 		
 		//int person#; // 특수문자 사용불가($, _ 제외)
-		int person;
+		//int person;
 		
-		int _person; // 특수문자 _이므로 사용가능
+		//int _person; // 특수문자 _이므로 사용가능
 		
 		// 공통 에러 코드 : Syntax error on token + "오류 원인", delete this token(권고내용)
 		 
--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -12,23 +12,23 @@ public class Ex08 {
 			(The local variable x may not have been initialized)
 		*/
 		
-		int x = 0;
-		int y = 0;
+		//int x = 0;
+		//int y = 0;
 		//or
 		
-		int X,Y;
-		X = 0;
-		Y = 0;
+		//int X,Y;
+		//X = 0;
+		//Y = 0;
 		
 		// 위 2가지 스타일로 정리할 수 있음
 		
 		
-		char grade = 'A'; // 문자 A를 char를 써서 grade에 대입하려면 작은따옴표 써야죠
+		//char grade = 'A'; // 문자 A를 char를 써서 grade에 대입하려면 작은따옴표 써야죠
 		
-		int salary = 2000000; // salary에 2,000,000을 대입인데 ,는 빼세요(정수값)
+		//int salary = 2000000; // salary에 2,000,000을 대입인데 ,는 빼세요(정수값)
 		
 		//byte n = 1000; // n에 1000을 대입인데 byte 수용범위는 -128 ~ 127임
-		int n = 1000; // 정교한 메모리 작업을 원하면 short도 가능 하지만 굳이? / long + L도 가능(메모리 사치)
+		//int n = 1000; // 정교한 메모리 작업을 원하면 short도 가능 하지만 굳이? / long + L도 가능(메모리 사치)
 		
 	}
 	
